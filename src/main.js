a533f06db408ebb51d591e340a9fe5f0fb2b5847
david35008 <67857450+david35008@users.noreply.github.com>
2020-07-19 21:46:20 +0300

    Update main.js
    Add comments to the script.

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ const the_List = document.getElementById('tasksList');
 const priority_Choose = document.getElementById('prioritySelector');
 const do_List = [];
 
+// add event to the "add" button:
 my_Button.addEventListener('click', addTask);
 
+// add task to the do_List array and add it to the dispaly:
 function addTask() {
     const box_Value = input_Box.value;
     const priority_Num = priority_Choose.value;
     if (box_Value !== "" && priority_Num !== "--") {
-        obj = {
+        let obj = {
             priority_Num: priority_Num,
             date: formatDate(new Date()),
             box_Value: box_Value
@@ -23,8 +25,10 @@ function addTask() {
     };
 };
 
+// add zero padding to 2 digits numbers:
 function pad(num) { return ('00' + num).slice(-2) };
 
+// change the date to SQL date format:
 function formatDate(date) {
     dateStr = date.getUTCFullYear() + '-' +
         pad(date.getUTCMonth() + 1) + '-' +
@@ -35,6 +39,7 @@ function formatDate(date) {
     return dateStr;
 };
 
+// add line to display:
 function print_line(obj) {
     const div_Create1 = addChild(the_List, "todoContainer");
     addChild(div_Create1, "todoPriority", obj.priority_Num);
@@ -42,6 +47,7 @@ function print_line(obj) {
     addChild(div_Create1, "todoText", obj.box_Value);
 };
 
+// add div child:
 function addChild(parent, className, text) {
     let div_Create = document.createElement('div');
     parent.appendChild(div_Create);
@@ -50,20 +56,23 @@ function addChild(parent, className, text) {
     return div_Create;
 };
 
+// add event to the "sort" button:
 const order_Button = document.getElementById('sortButton');
 order_Button.addEventListener('click', orderList);
 
+// order the tasks list by priority:
 function orderList() {
     do_List.sort(function (a, b) {
         return +a.priority_Num - +b.priority_Num;
     });
     cleanUl(the_List);
 
-    for ( let i = do_List.length - 1 ; i >= 0 ; i-- ) {
+    for (let i = do_List.length - 1; i >= 0; i--) {
         print_line(do_List[i]);
     };
 };
 
+// remove all child nodes of a list:
 function cleanUl(the_List) {
     while (the_List.hasChildNodes()) {
         the_List.removeChild(the_List.firstChild);
